51cdd0d19676e4d9dd3f38f8a18f5031a886e17b
Ramkumar S <ramkumar@Ramkumars-MacBook-Pro.local>
2022-08-13 05:14:37 +0530

    Initial commit

---
+++ src/main/java/com/auditPortal/io/UsersRepository.java
@@ -0,0 +1,9 @@
+package com.auditPortal.io;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsersRepository extends PagingAndSortingRepository<UserEntity, Long> {
+    UserEntity findByEmail(String email);
+}
